5defd18c7c612618db42d6df7cf9cad1d533ae23
imlidya <imlidya18@gmail.com>
2021-12-19 21:02:26 +0700

    basic automation positive case

---
+++ Cermati/Test Suites/Register/PositiveCase.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>PositiveCase</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e80832eb-fc50-48eb-96fe-b1c739992d1b</testSuiteGuid>
+   <testCaseLink>
+      <guid>e153b5a7-7964-465d-ad31-516b0de736b2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Register/PositiveCase</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
